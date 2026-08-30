daa1f6bac33b3be9aab6983dd707da21fca45f5f
Cornerstone SIS <dev@cornerstonelabs.org>
2026-08-30 11:00:56 +0000

    Disable Vite during feature tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Feature tests render Inertia responses before CI builds frontend assets.
+        $this->withoutVite();
+    }
 }
